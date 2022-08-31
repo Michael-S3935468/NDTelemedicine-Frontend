60437a272fa6a6f5c3b0bd1737b0480f9f8c7e22
s3899439 <s3899439@student.rmit.edu.au>
2022-08-31 17:59:44 +1000

    Adds home page header and medication list, Fixes drawer to display sign out at the bottom

--- ndtelemedicine/lib/main.dart
+++ ndtelemedicine/lib/main.dart
@@ -36,65 +36,105 @@ class MyHomePage extends StatelessWidget {
           IconButton(onPressed: () {}, icon: const Icon(Icons.notifications)),
         ],
       ),
-      body: const Center(
-        child: Text('ND Telemedicine'),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(height: 25),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              SizedBox(width: 25),
+              Text('Home',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 32,
+                  fontFamily: 'Inter',
+                )
+              ),
+            ]
+          ),
+          const Divider(
+            thickness: 2,
+            color: Color.fromRGBO(112, 112, 112, 1),
+            indent: 25,
+            endIndent: 25,
+          ),
+          const SizedBox(height: 25),
+          const SizedBox(width: 25),
+          const ExpansionTile(
+            leading: Icon(Icons.sentiment_very_satisfied),
+            title: Text('Medication for Today',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontFamily: 'Inter',
+                )
+            ),
+            children: <Widget>[
+              ListTile(title: Text('XYZ')),
+            ],
+          ),
+        ],
       ),
       drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue,),
-              child: Text('Drawer Header'),
-            ),
+
+        //drawer Code
+        width: MediaQuery.of(context).size.width * 0.77,
+        child: Column(
+
+        children: <Widget>[
             ListTile(
-              leading: const Icon(Icons.home,),
-              title: const Text('Home'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.account_circle,),
-              title: const Text('Profile'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.flag,),
-              title: const Text('Symptoms'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            Container(
-              child: Column(
-                children: const <Widget>[
-                  Divider(),
-                  ListTile(
-                    leading: Icon(Icons.logout),
-                    // tileColor: Colors.redAccent,
-                    title: Text('Sign Out'),),
-                ],
+              leading: const Icon(Icons.menu, color: Colors.white),
+              tileColor: const Color.fromRGBO(21, 101, 192, 1),
+              dense: true,
+              visualDensity: const VisualDensity(vertical: 2),
+              onTap: () {},
+              title: SizedBox(
+                height: 30,
+                child:  Image.asset('images/Header-Base.png', alignment: Alignment.centerRight,),
               ),
-            )
+            ),
+            const Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {},
+            ),
+            const Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              leading: const Icon(Icons.account_circle),
+              title: const Text('Profile'),
+              onTap: () {},
+            ),
+            const Divider(
+              color: Colors.white,
+            ),
+            ListTile(
+              leading: const Icon(Icons.flag),
+              title: const Text('Symptoms'),
+              onTap: () {},
+            ),
+            const Divider(
+              color: Colors.white,
+            ),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ListTile(
+                  leading: const Icon(Icons.logout),
+                  title: const Text('Sign Out'),
+                  onTap: () {},
+                ),
+              ),
+            ),
+            const SizedBox(height: 10,)
           ],
         ),
       ),
     );
   }
 }
+
