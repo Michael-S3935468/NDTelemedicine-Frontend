e6282ad015a376572b8fd509442fea3689c31d57
s3897812 <82488888+s3897812@users.noreply.github.com>
2022-09-12 14:02:15 +1000

    changes icon to home, adds text to show user is in Notif page

--- ndtelemedicine/lib/notif.dart
+++ ndtelemedicine/lib/notif.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:ndtelemedicine/main.dart';
 
@@ -7,16 +9,13 @@ class NotificationPage extends StatelessWidget {
   static const appTitle = 'NDTelemedicine';
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return const MaterialApp(
       title: appTitle,
       home: _NotificationPage(),
       debugShowCheckedModeBanner: false,
     );
-
   }
-
-
 }
 
 class _NotificationPage extends StatelessWidget {
@@ -24,26 +23,60 @@ class _NotificationPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<int> items = List<int>.generate(3, (int index) => index);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromRGBO(21, 101, 192, 1),
         title: SizedBox(
           height: 30,
-          child:  Image.asset('images/Header-Base.png'),
+          child: Image.asset('images/Header-Base.png'),
         ),
         centerTitle: true,
-        leading: 
-          IconButton(onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => MyHomePage(title: "ND Telemedicine"),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.notifications)
-        ),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(title: "ND Telemedicine"),
+                ),
+              );
+            },
+            icon: const Icon(Icons.home)),
+      ),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(height: 25),
+          Row(mainAxisAlignment: MainAxisAlignment.start, children: const [
+            SizedBox(width: 25),
+            Text('Recent Notifications',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 32,
+                  fontFamily: 'Inter',
+                )),
+          ]),
+          const Divider(
+            thickness: 2,
+            color: Color.fromRGBO(112, 112, 112, 1),
+            indent: 25,
+            endIndent: 25,
+          ),
+          const SizedBox(height: 25),
+          const SizedBox(width: 25),
+          const ExpansionTile(
+            leading: Icon(Icons.sentiment_very_satisfied),
+            title: Text('Medication for Today',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontFamily: 'Inter',
+                )),
+            children: <Widget>[
+              ListTile(title: Text('XYZ')),
+            ],
+          ),
+        ],
       ),
     );
   }
-
 }
