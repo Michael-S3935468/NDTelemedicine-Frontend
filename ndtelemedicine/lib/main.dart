0bde396622cf14abb7767326a3f2e432bb05f886
s3899439 <s3899439@student.rmit.edu.au>
2022-09-16 17:11:02 +1000

    enables drawer like to profile to navigate to the profile page

--- ndtelemedicine/lib/main.dart
+++ ndtelemedicine/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'profile/profile.dart';
 
 void main() => runApp(const MyApp());
 
@@ -107,7 +108,11 @@ class MyHomePage extends StatelessWidget {
             ListTile(
               leading: const Icon(Icons.account_circle),
               title: const Text('Profile'),
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => ProfilePage())
+                );
+              },
             ),
             const Divider(
               color: Colors.white,
