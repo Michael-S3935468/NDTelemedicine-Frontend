624cc18a0100c6ed2fd9717a3617afbe30918181
s3897812 <s3897812@student.rmit.edu.au>
2022-09-16 16:57:38 +1000

    implements visibility button for password text field

--- ndtelemedicine/lib/login.dart
+++ ndtelemedicine/lib/login.dart
@@ -8,17 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(21, 101, 192, 1),
-          title: SizedBox(
-            height: 30,
-            child: Image.asset('images/Header-Base.png'),
-          ),
-          centerTitle: true,
-        ),
-        body: const LoginPage(),
-      ),
+      home: const LoginPage(),
     );
   }
 }
@@ -33,117 +23,135 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  bool _isObscure = true;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      // body: SingleChildScrollView(
-      child: ListView(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            child: const Text(
-              "Login",
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 10),
-            child: const Text("Email Address"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: nameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "example@domain.net",
-                hintText: "Enter valid email",
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 10),
-            child: const Text("Password"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Enter password here",
-                  hintText: "Enter secure password"),
-            ),
-          ),
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => MyHomePage(
-                          title: "test",
-                        )));
-              },
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: const Color.fromRGBO(21, 101, 192, 1),
+        title: SizedBox(
+          height: 30,
+          child: Image.asset('images/Header-Base.png'),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(10),
               child: const Text(
                 "Login",
-                style: TextStyle(color: Colors.white, fontSize: 25),
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500),
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 10),
-            child: const Text("testing"),
-          ),
-          Container(
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(20),
+            Container(
+              padding: const EdgeInsets.only(left: 10),
+              child: const Text("Email Address"),
             ),
-            child: ElevatedButton(
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Thanks!'),
-                        content: Text(
-                            'You typed "${nameController.text}", the password was  "${passwordController.text}" '),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      );
-                    });
-                // Navigator.of(context).push(MaterialPageRoute(
-                //     builder: (context) => MyHomePage(
-                //       title: "test",
-                //     )));
-              },
-              child: const Text(
-                "test",
-                style: TextStyle(color: Colors.white, fontSize: 25),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "example@domain.net",
+                  hintText: "Enter valid email",
+                ),
               ),
             ),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.only(left: 10),
+              child: const Text("Password"),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: passwordController,
+                obscureText: _isObscure,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Enter password here",
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                          _isObscure ? Icons.visibility_off : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                    ),
+                    hintText: "Enter secure password"),
+              ),
+            ),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => MyHomePage(
+                            title: "test",
+                          )));
+                },
+                child: const Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 10),
+              child: const Text("testing"),
+            ),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Thanks!'),
+                          content: Text(
+                              'You typed "${nameController.text}", the password was  "${passwordController.text}" '),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: const Text('OK'),
+                            ),
+                          ],
+                        );
+                      });
+                },
+                child: const Text(
+                  "test",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+          ],
+        ),
+        // ),
       ),
-      // ),
     );
+
+    ;
   }
 }
