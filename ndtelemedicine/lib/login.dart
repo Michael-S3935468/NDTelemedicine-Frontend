5499ce819117bfcdc519edf65097f5750ff06c7c
s3897812 <82488888+s3897812@users.noreply.github.com>
2022-09-17 14:50:21 +1000

    implements basic validation checking (if form is empty or not) before login

--- ndtelemedicine/lib/login.dart
+++ ndtelemedicine/lib/login.dart
@@ -23,6 +23,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _LoginKey = GlobalKey<FormState>();
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   bool _isObscure = true;
@@ -48,52 +49,86 @@ class _LoginPageState extends State<LoginPage> {
             alignment: Alignment.center,
           ),
         ),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: ListView(
-            children: <Widget>[
-              Container(
-                alignment: Alignment.center,
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
-                child: const Text(
-                  "Login",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 10),
-                child: const Text("Email Address"),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    border: UnderlineInputBorder(),
-                    labelText: "example@domain.net",
-                    hintText: "Enter valid email",
+        child: Form(
+          key: _LoginKey,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.center,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 10),
-                child: const Text("Password"),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: _isObscure,
-                  decoration: InputDecoration(
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: const Text("Email Address"),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  // child: TextField(
+                  //   controller: nameController,
+                  //   decoration: const InputDecoration(
+                  //     filled: true,
+                  //     fillColor: Colors.white,
+                  //     border: UnderlineInputBorder(),
+                  //     labelText: "example@domain.net",
+                  //     hintText: "Enter valid email",
+                  //   ),
+                  // ),
+                  child: TextFormField(
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: "example@domain.net"),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Enter Valid E-mail!";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: const Text("Password"),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  // child: TextField(
+                  //   controller: passwordController,
+                  //   obscureText: _isObscure,
+                  //   decoration: InputDecoration(
+                  //       filled: true,
+                  //       fillColor: Colors.white,
+                  //       border: const UnderlineInputBorder(),
+                  //       labelText: "Enter password here",
+                  //       suffixIcon: IconButton(
+                  //         icon: Icon(_isObscure
+                  //             ? Icons.visibility_off
+                  //             : Icons.visibility),
+                  //         onPressed: () {
+                  //           setState(() {
+                  //             _isObscure = !_isObscure;
+                  //           });
+                  //         },
+                  //       ),
+                  //       hintText: "Enter secure password"),
+                  // ),
+                  child: TextFormField(
+                    controller: passwordController,
+                    obscureText: _isObscure,
+                    decoration: InputDecoration(
                       filled: true,
                       fillColor: Colors.white,
-                      border: const UnderlineInputBorder(),
                       labelText: "Enter password here",
                       suffixIcon: IconButton(
                         icon: Icon(_isObscure
@@ -105,85 +140,107 @@ class _LoginPageState extends State<LoginPage> {
                           });
                         },
                       ),
-                      hintText: "Enter secure password"),
-                ),
-              ),
-              Column(children: <Widget>[
-                Container(
-                  height: 50,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const MyHomePage(
-                                title: "NDTelemedicine",
-                              )));
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Enter valid password";
+                      }
+                      return null;
                     },
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+                Column(children: <Widget>[
+                  Container(
+                    height: 50,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                      // onPressed: () {
+                      // Navigator.of(context).push(MaterialPageRoute(
+                      //     builder: (context) => const MyHomePage(
+                      //       title: "NDTelemedicine",
+                      //     )));
+                      //
+                      // },
+
+                      // Validation Check
+                      onPressed: () {
+                        if (_LoginKey.currentState!.validate()) {
+                          //If the form is valid, login is successful and proceed to home page
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const MyHomePage(
+                                    title: "NDTelemedicine",
+                                  )));
+                        }
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
                     ),
                   ),
-                ),
-                Row(children: <Widget>[
-                  Expanded(
-                    child: Container(
-                        margin: const EdgeInsets.only(left: 10.0, right: 20.0),
-                        child: const Divider(
-                          color: Colors.black,
-                          height: 36,
-                        )),
-                  ),
-                  const Text("OR"),
-                  Expanded(
-                    child: Container(
-                        margin: const EdgeInsets.only(left: 20.0, right: 10.0),
-                        child: const Divider(
-                          color: Colors.black,
-                          height: 36,
-                        )),
+                  Row(children: <Widget>[
+                    Expanded(
+                      child: Container(
+                          margin:
+                              const EdgeInsets.only(left: 10.0, right: 20.0),
+                          child: const Divider(
+                            color: Colors.black,
+                            height: 36,
+                          )),
+                    ),
+                    const Text("OR"),
+                    Expanded(
+                      child: Container(
+                          margin:
+                              const EdgeInsets.only(left: 20.0, right: 10.0),
+                          child: const Divider(
+                            color: Colors.black,
+                            height: 36,
+                          )),
+                    ),
+                  ]),
+                  Container(
+                    height: 50,
+                    width: 350,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Thanks!'),
+                                content: Text(
+                                    'You typed "${nameController.text}", the password was  "${passwordController.text}" '),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: const Text('OK'),
+                                  ),
+                                ],
+                              );
+                            });
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blueGrey),
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
+                    ),
                   ),
                 ]),
-                Container(
-                  height: 50,
-                  width: 350,
-                  decoration: BoxDecoration(
-                    color: Colors.blueGrey,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Thanks!'),
-                              content: Text(
-                                  'You typed "${nameController.text}", the password was  "${passwordController.text}" '),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
-                            );
-                          });
-                    },
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blueGrey),
-                    child: const Text(
-                      "Sign Up",
-                      style: TextStyle(color: Colors.white, fontSize: 25),
-                    ),
-                  ),
-                ),
-              ]),
-            ],
+              ],
+            ),
           ),
         ),
       ),
