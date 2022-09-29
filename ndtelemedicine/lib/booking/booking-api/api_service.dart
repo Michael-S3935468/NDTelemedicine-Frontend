ebc6d8d82a5f05643e4b612b48e65d86d786dd4f
s3899439 <s3899439@student.rmit.edu.au>
2022-09-29 15:38:29 +1000

    adds header tag to enable Cross-origin resource sharing

--- ndtelemedicine/lib/booking/booking-api/api_service.dart
+++ ndtelemedicine/lib/booking/booking-api/api_service.dart
@@ -5,17 +5,22 @@ class BookingAPI {
   Future<List<Appointment>?> getAppointments(DateTime date) async {
     try {
       // Call Microservice for available appointments
-      // TODO: Add endpoint uri
-      var url = Uri.parse("PUT API ENDPOINT HERE" "/$date");
-      var response = await http.get(url);
+      // TODO: Add endpoint uri, using a different project to test json response body format
+      var url = Uri.parse('http://localhost:8080/item/time');
+      // var url = Uri.parse("PUT API ENDPOINT HERE" "/$date");
+      var response = await http.get(url, headers: {
+        "Access-Control_Allow_Origin": "*"
+      });
 
       // If response returned, make the appointment list
       if (response.statusCode == 200) {
-        AvailableTimes availableTimes = availableTimesFromJson(response.body);
+
+        // Temporary solution with fixed time slots, used for testing functionality, currently working as intended
+        List<List<String>> availableTimes =  availableTimesFromJson(response.body);
         List<Appointment> appointments = [];
 
         // Convert the available time slots into booking appointments
-        for (var t in availableTimes.times) {
+        for (var t in availableTimes) {
           List<Appointment> temp = fromTime(t[0], t[1], date);
           appointments += temp;
         }
@@ -25,5 +30,6 @@ class BookingAPI {
     } catch (e) {
       print(e);
     }
+    return null;
   }
 }
