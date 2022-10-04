4e07beb7e89ecd7775d5f5902af192c4148719c0
s3899439 <s3899439@student.rmit.edu.au>
2022-10-04 20:52:31 +1100

    Adds user to LoginResponse

--- ndtelemedicine/lib/login/model/LoginResponse.dart
+++ ndtelemedicine/lib/login/model/LoginResponse.dart
@@ -7,18 +7,21 @@ class LoginResponse {
     required this.success,
     required this.error,
     required this.reason,
-    this.token
+    this.token,
+    // this.user
   });
     bool success;
     String error;
     String reason;
     String? token;
+    // User? user;
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
     success: json["success"],
     error: json["error"],
     reason: json["reason"],
-    token: json["token"]
+    token: json["token"],
+    // user: User.fromJson(json["user"]),
   );
 
 }
