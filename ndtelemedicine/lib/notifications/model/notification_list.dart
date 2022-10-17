e814372dac91b2188756154aabbdf50489eda79d
s3897812 <s3897812@student.rmit.edu.au>
2022-10-17 14:13:06 +1100

    fixes typo

--- ndtelemedicine/lib/notifications/model/notification_list.dart
+++ ndtelemedicine/lib/notifications/model/notification_list.dart
@@ -7,7 +7,8 @@ class Notif {
   static List<Notif> notifList = [
     Notif(title: "Appointment", desc: "Today, 1:30pm - Dr. N. Riveria"),
     Notif(title: "Medication", desc: "Today, 4:00pm - 1x Lipitor"),
-    Notif(title: "Appointment", desc: "Tommorow, 11:30am - Dr. R. Giambrone"),
+    Notif(title: "Medication", desc: "Tomorrow, 8:00am - 1x Lipitor"),
+    Notif(title: "Appointment", desc: "Tomorrow, 11:30am - Dr. R. Giambrone"),
   ];
 
   static List<Notif> getNotif() {
