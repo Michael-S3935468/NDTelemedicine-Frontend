303640ddcd9810bb4767c5dca4f184f4c47cdc73
s3899439 <s3899439@student.rmit.edu.au>
2022-10-17 16:00:02 +1100

    changes response depending on the status code

--- ndtelemedicine/lib/login/login-api/api_service.dart
+++ ndtelemedicine/lib/login/login-api/api_service.dart
@@ -1,23 +1,23 @@
-import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import '../model/LoginResponse.dart';
 import '../model/login.dart';
 
-class LoginAPI{
+class LoginAPI {
   Future<LoginResponse> sendLogin(LoginRequest request) async {
-    var url = Uri.parse('http://localhost:8080/item/time');
-    final http.Response response = await http.post(
-        url,
-      headers: {
-        "Access-Control_Allow_Origin": "*",
-        "Content-Type": "application/json; charset=UTF-8",
-      },
-      body: loginRequestToJson(request)
-    );
+    var url = Uri.parse('http://localhost:8080/api/users/login');
+    var response = await http.post(url,
+        headers: {
+          HttpHeaders.accessControlAllowOriginHeader: "*",
+          HttpHeaders.contentTypeHeader: "application/json"
+        },
+        body: loginRequestToJson(request));
 
-    if (response.statusCode == 201) {
+    if (response.statusCode == 200) {
       return loginResponseFromJson(response.body);
+    } else if (response.statusCode == 401) {
+      return loginResponseFailFromJson(response.body);
     } else {
       throw Exception("Failed to login");
     }
--- ndtelemedicine/lib/login/model/LoginResponse.dart
+++ ndtelemedicine/lib/login/model/LoginResponse.dart
@@ -1,27 +1,37 @@
 import 'dart:convert';
 
+import '../../state_models/User.dart';
+
 LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
+LoginResponse loginResponseFailFromJson(String str) => LoginResponse.fromJsonFail(json.decode(str));
 
 class LoginResponse {
   LoginResponse({
     required this.success,
     required this.error,
     required this.reason,
-    this.token,
-    // this.user
+    required this.token,
+    required this.user
   });
     bool success;
-    String error;
-    String reason;
+    String? error;
+    String? reason;
     String? token;
-    // User? user;
+    User? user;
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
     success: json["success"],
     error: json["error"],
     reason: json["reason"],
     token: json["token"],
-    // user: User.fromJson(json["user"]),
+    user: User.fromJson(json["user"]),
   );
 
+  factory LoginResponse.fromJsonFail(Map<String, dynamic> json) => LoginResponse(
+      success: json["success"],
+      error: json["error"],
+      reason: json["reason"],
+      token: null,
+      user: null
+  );
 }
